8b410398702ae67cca6fc2cad719b21d79fefbf0
Jakob Jacobsen <58770803+Sezio27@users.noreply.github.com>
2025-10-07 20:17:21 +0200

    Refactor clients screen layout to slivers

--- lib/widgets/client_list.dart
+++ lib/widgets/client_list.dart
@@ -124,7 +124,10 @@ class _FullClientList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      padding: EdgeInsets.only(bottom: 16),
+      padding: const EdgeInsets.only(bottom: 16),
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      primary: false,
       itemCount: items.length,
       separatorBuilder: (_, __) => const SizedBox(height: 8),
       itemBuilder: (_, i) {
